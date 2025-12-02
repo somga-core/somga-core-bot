4db07fe35efff9f7cebc4197eee9f01974ceff8c
K0RSY <pgeorg2010@gmail.com>
2025-12-02 16:52:47 +0200

    Intresting changes

--- commands/intresting.py
+++ commands/intresting.py
@@ -8,7 +8,7 @@ def sex(user, args):
     wordle_answer = requests.get(wordle_url).json()["solution"]
 
     if not wordle_answer in args:
-        return "Вот это ты шалунишка!\n\nПеред тем как увидеть действие команды /sex, введи после команды ответ на сегодняшний wordle (Например: '/sex unity')"
+        return "Вот это ты шалунишка!\n\nПеред тем как увидеть действие команды /sex, введи после команды ответ на сегодняшний wordle (Например: /sex unity)"
     
     Users.send_data(user, {"sex_counter": Users.get_data(user, "sex_counter", 0) + 1})
     
@@ -18,8 +18,8 @@ def sex(user, args):
     }
 
 def sex_top(user, args):
-    if not "21" in args:
-        return
+    if not Users.get_data(user, "sex_password") in args:
+        return "Ты не признанный сексолог. Таким смотреть /sex_top нельзя\n\nПожалуйся, зарегистрируй себя как сексолог зарегистрируясь по команде: /sex_register password. Вместо password поставь свой пароль\n\n Потом, чтобы посмотреть топ, введи: /sex_top password. Вместо password опять введи свой пароль"
 
     users_sex = Users.get_data_from_all_users("sex_counter", 0)
     users_sex = dict(sorted(users_sex.items(), reverse=1, key=lambda x: x[1]))
@@ -35,3 +35,14 @@ def sex_top(user, args):
     return {
         "text": text
     } 
+
+def sex_register(user, args):
+    if len(args) == 0:
+        return "Ты не ввёл свой пароль. Введи: /sex_register password. Замени password на свой пароль"
+    
+    if not Users.get_data(user, "sex_password") is None:
+        return "Прости, но ты уже регистрировался как сексолог\n\nПароль поменять нельзя\n\nВообще никак"
+
+    Users.send_data(user, {"sex_password": args[0]})
+
+    return "Поздравляю, теперь ты признанный сексолог!"
